7ab7d33f0783cacd92e88b117cf41b1e4c1fcc15
Kevin Conti <Marketing.KevinConti@gmail.com>
2015-12-25 21:42:19 -0500

    Can add subs

--- init.py
+++ init.py
@@ -60,9 +60,33 @@ def remove_subs(config):
     empty_file.truncate()
     config.write(empty_file)
 
+def add_subs(config):
+    is_done = False
+    while is_done == False:
+        is_correct = False
+        while (is_correct == False):
+            print 'What is the sub you would like to add? (Enter just the subreddit name, without the /r/'
+            sub_name = raw_input()
+            print 'is', sub_name, 'correct? (y/n) '
+            is_correct = check_input()
+
+            if (is_correct):
+                is_correct = False
+                upvotes = -1
+                while is_correct == False:
+                    upvotes = int(raw_input('How many upvotes should it take for a post to qualify for your Summary? (Integers only)'))
+                    print 'is', upvotes, 'correct? (y/n) '
+                    is_correct = check_input()
+                config.set('Subs',sub_name, upvotes)
+        print 'Are you done adding subs? (y/n)'
+        is_done = check_input()
+    empty_file = open('config.ini', 'r+')
+    empty_file.truncate()
+    config.write(empty_file)
+
+
 #Iterates through current results, and then asks
 #if more should be added on
-
 def modify_subs(cfg_file, config):
     #TODO iterate through current results, and then allow for more
     #to be added on
@@ -71,6 +95,13 @@ def modify_subs(cfg_file, config):
     if to_remove:
         cfg_file.close()
         remove_subs(config)
+        cfg_file = open('config.ini', 'r+')
+    #Ask to add new subs
+    print 'Would you like to add new subs? (y/n) '
+    to_add = check_input()
+    if to_add:
+        cfg_file.close()
+        add_subs(config)
         cfg_file = open('config.ini', 'r+')
 
 print 'Would you like to modify your subs? (y/n) '
